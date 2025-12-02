b13569b4752275a92b132b9e5f5519f0b26c9a8f
deryalfi <deryalfif@gmail.com>
2025-12-02 11:09:03 +0000

    modified:   chatbot-streamlit.py 	modified:   requirements.txt

--- chatbot-streamlit.py
+++ chatbot-streamlit.py
@@ -4,10 +4,10 @@ from datetime import datetime
 import os
 from dotenv import load_dotenv
 import json
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.schema import Document
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
 import pickle
 
 # Load environment variables
